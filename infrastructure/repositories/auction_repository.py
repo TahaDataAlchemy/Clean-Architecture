4d0d4259b31b678cf5610c0d72f12d09da499862
Taha Mehboob <157484236+TahaDataAlchemy@users.noreply.github.com>
2025-03-05 12:49:00 -0800

    Add files via upload

---
+++ infrastructure/repositories/auction_repository.py
@@ -0,0 +1,14 @@
+from abc import abstractmethod,ABC
+from typing import Any
+
+from domain.entities.auction import Auction
+from domain.entities.bid import Bid
+
+class AuctionRepository(ABC):
+    @abstractmethod
+    async def get(self,**filters: Any) -> Auction|None:
+        pass
+
+    @abstractmethod
+    async def add_bid(self,bid:Bid) -> bool:
+        pass
